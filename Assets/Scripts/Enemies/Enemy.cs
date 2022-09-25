e784a2f3ee5e501cdba955713162d02b214eb15e
superpi2 <superpi2.Aa.Xx@gmail.com>
2022-09-25 14:30:17 -0400

    Fixes

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,33 +4,37 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
-    public EnemyProperties properties; //max health, damage, etc
-    private int health;
-    public int Health => health;
-    
+    public float health = 1;
+
     // Start is called before the first frame update
     protected virtual void Start()
     {
-        //basic enemy initialization
 
-        
+    }
+
+    protected virtual void Update()
+    {
+
     }
 
     protected abstract void Attack();
-    protected virtual void Move() {
+    protected virtual void Move()
+    {
         //pathfinding shiz, possibly do setup in here and then specific behaviors in derived class
 
     }
 
-    protected virtual void Die() {
+    protected virtual void Die()
+    {
         Destroy(this);
     }
 
-    public virtual void TakeDamage(int damage) {
+    public virtual void TakeDamage(int damage)
+    {
         health -= damage;
-        if(health <= 0) {
+        if (health <= 0)
+        {
             Die();
         }
     }
-
 }
--- Assets/Scripts/Enemies/HomingEnemy.cs
+++ Assets/Scripts/Enemies/HomingEnemy.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
-namespace Enemies { //This is just for organization's sake
-    public class HomingEnemy : Enemy
+public class HomingEnemy : Enemy
+{
+    protected AIPath aiPath;
+    protected P1Controller target;
+
+
+    // Start is called before the first frame update
+    protected override void Start()
     {
-        // Start is called before the first frame update
-        protected override void Start()
-        {
-            
-        }
-        
-        protected override void Attack() {
-            
-        }
-        
+        aiPath = GetComponent<AIPath>();
+        target = FindObjectOfType<P1Controller>();
     }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        Move();
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+
+        aiPath.destination = target.transform.position;
+    }
+
+    protected override void Attack()
+    {
+
+    }
+
 }
