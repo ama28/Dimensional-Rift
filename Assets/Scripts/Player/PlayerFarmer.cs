41979aae563ce8c95a21a7727bafabdf83f8853e
ama28 <amm28y@gmail.com>
2022-09-25 15:36:15 -0400

    files organization

---
+++ Assets/Scripts/Player/PlayerFarmer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFarmer : MonoBehaviour
+{
+    public float moveSpeed;
+    public float curHealth;
+    public float maxHealth;
+}
