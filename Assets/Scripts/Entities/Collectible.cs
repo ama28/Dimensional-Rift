bf8c5f36a5bbcd5c882c9afd82c3fb6a1c57493f
Will Ozeas <willatme@me.com>
2022-10-02 14:44:14 -0400

    Coin spawner in main scene and knockback bug fix

--- Assets/Scripts/Entities/Collectible.cs
+++ Assets/Scripts/Entities/Collectible.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    public virtual void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
             Destroy(gameObject);
