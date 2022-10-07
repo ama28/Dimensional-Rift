c1cd8c4e5aa5d359cd9c1ad2394d266cd58c813c
superpi2 <superpi2.Aa.Xx@gmail.com>
2022-10-07 13:02:50 -0400

    More bullet changes, early farmer animations

--- Assets/Scripts/Entities/Bullet.cs
+++ Assets/Scripts/Entities/Bullet.cs
@@ -10,21 +10,44 @@ public class Bullet : MonoBehaviour
     public Being source; //should be set when fired
 
     // fancy parameter things
+    [Tooltip("How fast the bullet moves.")]
     public float speed;
+    [Tooltip("How far the bullet can travel before it despawns.")]
     public float range = 0f;
     [SerializeField]
+    [Tooltip("How much damage the bullet will deal. If damageDamping " +
+        "is enabled, this is the point-blank damage.")]
     private float damage = 1f;
     [SerializeField]
+    [Tooltip("How much knockback the target will take when hit. " +
+        "Set to 0 to disable knockback.")]
     private float knockback;
     [SerializeField]
+    [Tooltip("The number of enemies the bullet can travel through " +
+        "before despawning.")]
     private int pierce = 1;
     [SerializeField]
-    private float splashRange = 0.0f; 
+    private float splashRange = 0.0f;
+    [SerializeField]
+    [Tooltip("Wether or not to reduce bullet damage proportional " +
+        "to lifetime remaining.")]
+    private bool damageDamping = false;
+
+    private Vector3 pos0;
 
     // Start is called before the first frame update
     protected virtual void Start()
     {
+        pos0 = transform.position;
         SetHitInfo();
+    }
+
+    protected void Update()
+    {
+        float distanceTraveled = Vector3.Magnitude(pos0 - transform.position);
+
+        if (distanceTraveled > range)
+            Destroy(gameObject);
     }
 
     public virtual void SetUpBullet(Being source, GunInfo gunInfo) {
@@ -38,7 +61,14 @@ public class Bullet : MonoBehaviour
     }
 
     protected virtual void SetHitInfo() {
-        hitInfo.damage = damage;
+        if (damageDamping)
+        {
+            float dampFactor = 1 - (Vector3.Magnitude(pos0 -
+                transform.position) / range);
+            hitInfo.damage = damage * dampFactor;
+        } else
+            hitInfo.damage = damage;
+
         hitInfo.source = source;
         hitInfo.sourceTransform = transform;
         hitInfo.knockbackScalar = knockback;
@@ -50,7 +80,7 @@ public class Bullet : MonoBehaviour
 
         if (other.layer == LayerMask.NameToLayer("Wall"))
         {
-            // shot a wall :/
+            // shot a wall :(
             Destroy(gameObject);
             return;
         }
