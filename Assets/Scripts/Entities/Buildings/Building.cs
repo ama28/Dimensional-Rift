5f95b411aaf597f67672dec1a97e39d6c9f03e48
Will Ozeas <willatme@me.com>
2022-10-09 16:55:59 -0400

    Building setup & mouse angle fix

---
+++ Assets/Scripts/Entities/Buildings/Building.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : Being
+{
+    public Vector2Int coordinates; //within the grid, the bottom left square
+    public Vector2Int size;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
