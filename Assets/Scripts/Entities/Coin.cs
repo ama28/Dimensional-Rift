e3a9cdb2e52a24d5b67c3662e99594ed7ff9f7fc
Will Ozeas <willatme@me.com>
2022-10-22 14:26:00 -0400

    Coins working

--- Assets/Scripts/Entities/Coin.cs
+++ Assets/Scripts/Entities/Coin.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 
 public class Coin : Collectible
 {
+    private CoinSpawnHandler coinSpawnHandler;
 
+    void Start() {
+        coinSpawnHandler = GameObject.Find("CoinSpawner").GetComponent<CoinSpawnHandler>();
+    }
     public override void OnTriggerEnter2D(Collider2D c){
         // If player collects coin
         if(c.gameObject.tag == "PlayerFarmer"){
-            GameObject.Find("CoinSpawner").GetComponent<CoinSpawnHandler>().SpawnCoin(); // Spawn new coin
-            GameObject.Find("CoinSpawner").GetComponent<CoinSpawnHandler>().DecrementCoinCount(); // Decrement coin
+            coinSpawnHandler.SpawnCoin(); // Spawn new coin
+            coinSpawnHandler.OnCollect(); // Decrement coin
+            GameManager.Instance.currency++;
         }
 
         //destroys coin
