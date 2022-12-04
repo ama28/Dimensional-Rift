f3f265a3d0f42d78502cc1da57e46f25d2deaa38
Oodbirds <90927949+Oodbirds@users.noreply.github.com>
2022-12-04 17:24:34 -0500

    cutscene playing without sound

--- Assets/Scripts/UI/Pause Menu/PlayButton.cs
+++ Assets/Scripts/UI/Pause Menu/PlayButton.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 namespace UI {
     public class PlayButton : MonoBehaviour
     {
+        public GameObject videoPlayer;
+        public void playVideo()
+        {
+            videoPlayer.SetActive(true);
+            print("playing video" + videoPlayer);
+            /*videoPlayer = GetComponent<VideoPlayer>();*/
+            videoPlayer.GetComponent<VideoPlayer>().Play();
+        }
         public void Play() {
             AudioManager.Instance.Click();
             GameManager.Instance.StartGame();
